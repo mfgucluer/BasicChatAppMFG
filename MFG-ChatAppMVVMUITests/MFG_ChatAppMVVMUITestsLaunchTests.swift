628b3714937381a552b29bb84bcba3c2346c2184
Mustafa Fatih GUCLUER <fatihgucluer@hotmail.com>
2024-09-04 13:22:38 +0300

    Initial Commit

---
+++ MFG-ChatAppMVVMUITests/MFG_ChatAppMVVMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MFG_ChatAppMVVMUITestsLaunchTests.swift
+//  MFG-ChatAppMVVMUITests
+//
+//  Created by Mustafa Fatih on 4/9/24.
+//
+
+import XCTest
+
+final class MFG_ChatAppMVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
